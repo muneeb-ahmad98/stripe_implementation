9b6785e72620b18cfe293350b48e23974fccd280
Muneeb Ahmad <muneeb.panwar@gmail.com>
2022-12-25 23:15:19 +0500

    Configured stripe event gem

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,4 @@
 Stripe.api_key = Rails.application.credentials.stripe_secret_key
+StripeEvent.signing_secret = Rails.application.credentials.stripe[:signing_secret]
+StripeEvent.configure do |events|
+end
